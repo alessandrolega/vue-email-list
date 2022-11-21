7bd0b9c601b4553a841ec48e86aef0543777baf5
alessandrolega <alessandrolega24@gmail.com>
2022-11-22 00:32:50 +0100

    pushate emails da API in array dataEmails creato

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 var app = new Vue({
     el: '#app',
     data: {
-     
+        dataEmails: [],
+        
+        
+    },
+    mounted() {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                console.log(response.data.response)
+                let responseData = response.data.response;
+                this.dataEmails.push(responseData);
+                console.log(this.dataEmails)
+                
+            })
+    },
+    methods: {
+
     }
   })
