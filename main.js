3435909e7eb50961d1dcfd91a27af6bf84e201b4
alessandrolega <alessandrolega24@gmail.com>
2022-11-22 00:54:16 +0100

    fix push da API in array = dentro funzione click

--- main.js
+++ main.js
@@ -5,17 +5,18 @@ var app = new Vue({
         
         
     },
-    mounted() {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((response) => {
-                console.log(response.data.response)
-                let responseData = response.data.response;
-                this.dataEmails.push(responseData);
-                console.log(this.dataEmails)
+    methods: {
+        generateEmail(){
+            for(i=0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    console.log(response.data.response)
+                    let responseData = response.data.response;
+                    this.dataEmails.push(responseData);
+                    console.log(this.dataEmails)
                 
             })
-    },
-    methods: {
-
+            }
+        }
     }
   })
